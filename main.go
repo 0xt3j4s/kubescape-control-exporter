44be8c5298f973989806aa4fa3367d0ab30bc892
0xt3j4s <jamdade.2@iitj.ac.in>
2023-08-08 11:04:47 +0530

    expose metrics to prometheus
    Signed-off-by: 0xt3j4s <jamdade.2@iitj.ac.in>

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"log"
+	"net/http"
 	"gopkg.in/yaml.v2"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // Summary is a struct that contains the summary of the YAML file
@@ -21,7 +25,7 @@ type Severities struct {
 	High     map[string]SeverityControls `yaml:"high"`
 	Medium   map[string]SeverityControls `yaml:"medium"`
 	Low      map[string]SeverityControls `yaml:"low"`
-	Other    map[string]SeverityControls `yaml:"other"`
+	Unknown   map[string]SeverityControls `yaml:"unknown"`
 }
 
 type SeverityControls struct {
@@ -50,9 +54,72 @@ func parseYAML(fileContent []byte) (*ControlsSummary, error) {
 	return &data.Spec, nil
 }
 
+// Define the metrics
+var (
+	controlsClusterCritical = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "controls_cluster_critical",
+			Help: "Number of critical severity controls in the cluster",
+		},
+		[]string{"scope", "severity"},)
+	
+	controlsClusterHigh = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "controls_cluster_high",
+			Help: "Number of high severity controls in the cluster",
+		},
+		[]string{"scope", "severity"},)
+	
+	controlsClusterMedium = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "controls_cluster_medium",
+			Help: "Number of medium severity controls in the cluster",
+		},
+		[]string{"scope", "severity"},)
+	
+	controlsClusterLow = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "controls_cluster_low",
+			Help: "Number of low severity controls in the cluster",
+		},
+		[]string{"scope", "severity"},)
+	
+	controlsClusterUnknown = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "controls_cluster_unknown",
+			Help: "Number of unknown severity controls in the cluster",
+		},
+		[]string{"scope", "severity"},)
+
+)
+
+func updateMetrics(summary ControlsSummary) {
+	for severity, controls := range summary.Severities.Critical {
+		controlsClusterCritical.WithLabelValues("Critical", severity).Add(float64(controls.All))
+	}
+	for severity, controls := range summary.Severities.High {
+		controlsClusterHigh.WithLabelValues("High", severity).Add(float64(controls.All))
+	}
+	for severity, controls := range summary.Severities.Medium {
+		controlsClusterMedium.WithLabelValues("Medium", severity).Add(float64(controls.All))
+	}
+	for severity, controls := range summary.Severities.Low {
+		controlsClusterLow.WithLabelValues("Low", severity).Add(float64(controls.All))
+	}
+	for severity, controls := range summary.Severities.Unknown {
+		controlsClusterUnknown.WithLabelValues("Unknown", severity).Add(float64(controls.All))
+	}
+}
+
 func main() {
+	prometheus.MustRegister(controlsClusterCritical)
+	prometheus.MustRegister(controlsClusterHigh)
+	prometheus.MustRegister(controlsClusterMedium)
+	prometheus.MustRegister(controlsClusterLow)
+	prometheus.MustRegister(controlsClusterUnknown)
+
 	// Read the sample YAML file
-	file, err := os.Open("test.yaml")
+	file, err := os.Open("VulnerabilityManifestSummary.yaml")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -78,7 +145,14 @@ func main() {
 	printControls(summary.Severities.High, "High")
 	printControls(summary.Severities.Medium, "Medium")
 	printControls(summary.Severities.Low, "Low")
-	printControls(summary.Severities.Other, "Other")
+	printControls(summary.Severities.Unknown, "Unknown")
+
+	updateMetrics(*summary)
+
+	// Expose metrics via HTTP
+	http.Handle("/metrics", promhttp.Handler())
+	fmt.Println("\nExporter is running at :8080/metrics")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func printControls(controlsMap map[string]SeverityControls, category string) {
